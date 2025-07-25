bf775afe37a1d7acbfff045d90b4af6d6c55e205
ibihafiz <ibihafiz@gmail.com>
2025-07-25 21:36:58 +0500

    Update build.sh

--- .render/build.sh
+++ .render/build.sh
@@ -1,2 +1,4 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y ffmpeg
+#!/bin/bash
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz | tar xJ
+mv ffmpeg-*/ffmpeg /usr/local/bin
+chmod +x /usr/local/bin/ffmpeg
